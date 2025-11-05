26deea6be0cd85a75a7b45b3dcd0d1db3457d9e3
tuna-guven <amnesty.precook334@addy.io>
2025-11-05 16:27:34 +0300

    training parameters increased

--- pytorch_train.py
+++ pytorch_train.py
@@ -6,7 +6,7 @@ import chess.pgn
 from pytorch_engine import PyTorchChessEngine, ChessModel, create_input_for_nn, encode_moves
 
 # --- 1. Load PGN games ---
-LIMIT_OF_GAMES = 10000
+LIMIT_OF_GAMES = 20000
 pgn_path = r"C:\Users\User\chess_project\data\lichess_elite\lichess_elite_2025-08.pgn"
 games = []
 
@@ -25,15 +25,14 @@ y, move_to_int = encode_moves(y_uci)
 
 # --- 3. Create engine and train model ---
 engine = PyTorchChessEngine(
-    is_player_black=False,
     model=ChessModel(num_classes=len(move_to_int)),
     move_to_int=move_to_int
 )
 
 engine.train_model(
     X, y,
-    epochs=5,
-    save_model_path=r".\models\TORCH_10EPOCHS.pth",
+    epochs=50,
+    save_model_path=r".\models\TORCH_50EPOCHS.pth",
     save_mapping_path=r".\models\move_to_int"
 )
 
